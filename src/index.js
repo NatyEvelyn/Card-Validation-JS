a2a5afe95eb5e955487930708ceb61f0e78ef89a
Natália Evelyn <natalia_evelyn@icloud.com>
2022-08-02 21:10:06 -0300

    Primeiro commit projeto Card Validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+
+let name = prompt("Bem vinda(o) a melhor loja de jogos! Por favor confirme seu nome:");
+document.getElementById("username").innerHTML = name;
+
+const validateButton = document.getElementById("btn-validate");
+const cardNum = document.getElementById("cardNumber");
+
+const regex = /[^0-9]./g;
+validateButton.addEventListener("click", cardValidation);
+cardNum.addEventListener("keyup", cloneCardNumber);
+
+cardNum.addEventListener("input", function () {
+  cardNum.value = cardNum.value.replace(regex, "");
+});
+
+function cloneCardNumber(e) {
+  e.preventDefault();
+  const value = document.getElementById("cardNumber").value;
+  const mask = validator.maskify(value);
+
+  document.getElementById("cardClone").value = mask;
+}
+
+function cardValidation(e) {
+  e.preventDefault();
+  const digValue = cardNum.value;
+  const finValidation = validator.isValid(digValue);
+
+  if (digValue === "0000000000000000" || digValue === "") {
+    return (document.getElementById("result").innerHTML =
+      "Por favor, digite o número do cartão!");
+  }
+  if (finValidation === true) {
+    return (document.getElementById("result").innerHTML =
+      "Cartão validado com sucesso! Download em andamento.");
+  }
+  if (finValidation === false) {
+    return (document.getElementById("result").innerHTML =
+      "Cartão inválido. Digite novamente!");
+  }
+}
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
-};
+  isValid(cardNumber) {
+    const cardArray = [];
+    for (let i = 0; i < cardNumber.length; i++) {
+      const digit = parseInt(cardNumber[i]);
+      cardArray.push(digit);
+    }
 
+    const cardNumReverse = cardArray.reverse();
+    for (let i = 0; i < cardNumReverse.length; i++) {
+      if ((i + 1) % 2 === 0) {
+        cardNumReverse[i] = cardNumReverse[i] * 2;
+      }
+      if (cardNumReverse[i] >= 10) {
+        cardNumReverse[i] = cardNumReverse[i] - 9;
+      }
+    }
+
+    let sumTotal = 0;
+    for (let i = 0; i < cardNumReverse.length; i++) {
+      sumTotal = sumTotal + cardNumReverse[i];
+    }
+    if (sumTotal % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify(cardNumber) {
+    let maskNum = [];
+
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i < cardNumber.length - 4) {
+        maskNum.push("#");
+      } else {
+        maskNum.push(cardNumber[i]);
+      }
+    }
+    const masked = maskNum.join("");
+    return masked;
+  },
+};
 export default validator;
